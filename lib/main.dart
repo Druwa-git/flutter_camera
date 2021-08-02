fdc62e8f27cf750865a428cb99e1b05afc475b76
druwa <wbsl0427@gmail.com>
2021-08-02 21:20:41 +0900

    make face detection by google ml kit

--- lib/main.dart
+++ lib/main.dart
@@ -1,190 +1,94 @@
-import 'dart:async';
 import 'dart:io';
 
 import 'package:camera/camera.dart';
+
+import 'face_detection.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_ml_vision/firebase_ml_vision.dart';
-import 'package:flutter/foundation.dart';
+
+List<CameraDescription> cameras = [];
 
 Future<void> main() async {
-  // Ensure that plugin services are initialized so that `availableCameras()`
-  // can be called before `runApp()`
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Obtain a list of the available cameras on the device.
-  final cameras = await availableCameras();
-
-  // Get a specific camera from the list of available cameras.
-  final firstCamera = cameras.first;
-
-  runApp(
-    MaterialApp(
-      theme: ThemeData.dark(),
-      home: TakePictureScreen(
-        // Pass the appropriate camera to the TakePictureScreen widget.
-        camera: firstCamera,
-      ),
-    ),
-  );
+  cameras = await availableCameras();
+  runApp(MyApp());
 }
 
-// A screen that allows users to take a picture using a given camera.
-class TakePictureScreen extends StatefulWidget {
-  const TakePictureScreen({
-    Key? key,
-    required this.camera,
-  }) : super(key: key);
-
-  final CameraDescription camera;
-
+class MyApp extends StatelessWidget {
   @override
-  TakePictureScreenState createState() => TakePictureScreenState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    );
+  }
 }
 
-class TakePictureScreenState extends State<TakePictureScreen> {
-  dynamic _scanResults;
-  late CameraController _controller;
-  late Future<void> _initializeControllerFuture;
-
-  bool _isDetecting = false;
-  CameraLensDirection _direction = CameraLensDirection.back;
-
-  @override
-  void initState() {
-    super.initState();
-    // To display the current output from the Camera,
-    // create a CameraController.
-    /*
-    _controller = CameraController(
-      // Get a specific camera from the list of available cameras.
-      widget.camera,
-      // Define the resolution to use.
-      ResolutionPreset.medium,
-    );
-     */
-
-    // Next, initialize the controller. This returns a Future.
-    _initializeControllerFuture = _initializeCamera();
-  }
-
-  Future<CameraDescription> _getCamera(CameraLensDirection dir) async {
-    return await availableCameras().then(
-          (List<CameraDescription> cameras) => cameras.firstWhere(
-            (CameraDescription camera) => camera.lensDirection == dir,
-      ),
-    );
-  }
-
-  Future<void> _initializeCamera() async {
-    _controller = CameraController(
-      await _getCamera(_direction),
-      /*
-      defaultTargetPlatform == TargetPlatform.iOS
-          ? ResolutionPreset.low
-          : ResolutionPreset.medium,
-      */
-        ResolutionPreset.medium
-    );
-    await _controller.initialize();
-
-    final faceDetector = FirebaseVision.instance.faceDetector();
-    //List<Face> faces = await faceDetector.processImage(image);
-
-    _controller.startImageStream((CameraImage image) {
-      if (_isDetecting) {
-        //Future<List<Face>> faces = faceDetector.processImage(image);
-        return;
-      }
-        _isDetecting = true;
-        try {
-          print(_isDetecting);
-          // await doOpenCVDectionHere(image)
-        } catch (e) {
-          print(_isDetecting);
-          //await handleExepction(e);
-        } finally {
-          _isDetecting = false;
-        }
-      }
-    );
-  }
-
-  @override
-  void dispose() {
-    // Dispose of the controller when the widget is disposed.
-    _controller.dispose();
-    super.dispose();
-  }
-
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Face Recognition')),
-      // You must wait until the controller is initialized before displaying the
-      // camera preview. Use a FutureBuilder to display a loading spinner until the
-      // controller has finished initializing.
-      body: FutureBuilder<void>(
-        future: _initializeControllerFuture,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            // If the Future is complete, display the preview.
-            return CameraPreview(_controller);
-          } else {
-            // Otherwise, display a loading indicator.
-            return const Center(child: CircularProgressIndicator());
-          }
-        },
+      appBar: AppBar(
+        title: Text('Google ML Kit Demo App'),
+        centerTitle: true,
+        elevation: 0,
       ),
-      /*
-      floatingActionButton: FloatingActionButton(
-        // Provide an onPressed callback.
-        onPressed: () async {
-          // Take the Picture in a try / catch block. If anything goes wrong,
-          // catch the error.
-          try {
-            // Ensure that the camera is initialized.
-            await _initializeControllerFuture;
-
-            // Attempt to take a picture and get the file `image`
-            // where it was saved.
-            final image = await _controller.takePicture();
-
-            // If the picture was taken, display it on a new screen.
-            await Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (context) => DisplayPictureScreen(
-                  // Pass the automatically generated path to
-                  // the DisplayPictureScreen widget.
-                  imagePath: image.path,
-                ),
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              child: Column(
+                children: [
+                  ExpansionTile(
+                    title: const Text("Vision"),
+                    children: [
+                      CustomCard(
+                        'Face Detector',
+                        FaceDetectorView(),
+                        featureCompleted: true,
+                      ),
+                    ],
+                  ),
+                ],
               ),
-            );
-          } catch (e) {
-            // If an error occurs, log the error to the console.
-            print(e);
-          }
-        },
-        child: const Icon(Icons.camera_alt),
+            ),
+          ),
+        ),
       ),
-      */
     );
   }
 }
 
-// A widget that displays the picture taken by the user.
-class DisplayPictureScreen extends StatelessWidget {
-  final String imagePath;
+class CustomCard extends StatelessWidget {
+  final String _label;
+  final Widget _viewPage;
+  final bool featureCompleted;
 
-  const DisplayPictureScreen({Key? key, required this.imagePath})
-      : super(key: key);
+  const CustomCard(this._label, this._viewPage,
+      {this.featureCompleted = false});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Display the Picture')),
-      // The image is stored as a file on the device. Use the `Image.file`
-      // constructor with the given path to display the image.
-      body: Image.file(File(imagePath)),
+    return Card(
+      elevation: 5,
+      margin: EdgeInsets.only(bottom: 10),
+      child: ListTile(
+        tileColor: Theme.of(context).primaryColor,
+        title: Text(
+          _label,
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        onTap: () {
+          if (Platform.isIOS && !featureCompleted) {
+            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                content: const Text(
+                    'This feature has not been implemented for iOS yet')));
+          } else
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => _viewPage));
+        },
+      ),
     );
   }
 }
